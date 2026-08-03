f28c26cd5a620e0c02bd4c45e6d85041d5163678
Suliman <sulimanrageb015@gmail.com>
2026-08-03 22:12:50 +0300

    Q01

---
+++ OOP Assignment/DeliveryAddress.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Assignment
+{
+    internal struct DeliveryAddress
+    {   public string City;
+        public string Street;
+    }
+}
--- OOP Assignment/Program.cs
+++ OOP Assignment/Program.cs
@@ -1,1 +1,44 @@
 ﻿
+//Part 01 : Theoretical Questions
+#region Question 1
+using OOP_Assignment;
+#region First Requirement
+
+//a)What happens when a DeliveryAddress variable is copied into another variable and the copy is modified?
+//DeliveryAddress address01 = new DeliveryAddress();
+//DeliveryAddress address02 = new DeliveryAddress();
+//address01.City = "cairo";
+//address01.Street = "street01";
+//Console.WriteLine("addres01...");
+//Console.WriteLine(address01.City);
+//Console.WriteLine(address01.Street);
+//address02= address01;
+//Console.WriteLine("addres02...");
+//Console.WriteLine(address02.City);
+//Console.WriteLine(address02.Street);
+//address02.City = "giza";
+//address02.Street = "street02";
+//Console.WriteLine("addres02...");
+//Console.WriteLine(address02.City);
+//Console.WriteLine(address02.Street);
+//Console.WriteLine("addres01...");
+//Console.WriteLine(address01.City);
+//Console.WriteLine(address01.Street);
+// ودا يثبت ان address01 الاصل م اتغيرش لأن struct Value type
+#endregion
+
+#region The second requirement
+//b) What happens when a Customer variable is copied into another variable and one variable modifies the object
+//Customer customer01 = new Customer();
+//Customer customer02 = new Customer();
+//customer01.Name = "Ahmed";
+//customer02 = customer01;
+//Console.WriteLine(customer01.Name);
+//Console.WriteLine(customer02.Name);
+//customer02.Name = "Suliman";
+//Console.WriteLine(customer01.Name);
+//Console.WriteLine(customer02.Name);
+// ودا يثبت ان customer01 الاصل اتغيرت لأنها class من نوع Referencetype
+#endregion
+#endregion
+
