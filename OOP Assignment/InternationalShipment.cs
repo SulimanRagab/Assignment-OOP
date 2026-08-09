8b992c4b3e5fc3d05fd563a2072336171b710b1b
Suliman <sulimanrageb015@gmail.com>
2026-08-09 16:47:15 +0300

    2. Create three shipment types (InternationalShipment).

---
+++ OOP Assignment/InternationalShipment.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Assignment
+{
+    internal class InternationalShipment : Shipment
+    {
+        private string destinationCountry;
+        private decimal customsFee;
+
+        public string DestinationCountry
+        {
+            get { return destinationCountry; }
+            private set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                {
+                    destinationCountry = value;
+                }
+            }
+        }
+
+        public decimal CustomsFee
+        {
+            get { return customsFee; }
+            private set
+            {
+                if (value >= 0)
+                {
+                    customsFee = value;
+                }
+            }
+        }
+
+        public override decimal EstimatedCost
+        {
+            get
+            {
+                return DeliveryFee + (Weight * 5) + customsFee;
+            }
+        }
+
+        public InternationalShipment(string trackingCode, string description, decimal weight, decimal deliveryFee, DeliveryAddress destination, string DestinationCountry, decimal CustomsFee) : base(trackingCode, description, weight, deliveryFee, destination)
+        {
+            DestinationCountry = destinationCountry;
+            CustomsFee = customsFee;
+        }
+    }
+}
+
