ad102e379080fb0f42b14f433e54fb13c1e4939d
Suliman <sulimanrageb015@gmail.com>
2026-08-09 16:46:18 +0300

    2. Create three shipment types (ExpressShipment ).

---
+++ OOP Assignment/ExpressShipment.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Assignment
+{
+    internal class ExpressShipment : Shipment
+    {
+        private decimal extraFee;
+
+        public decimal ExtraFee
+        {
+            get { return extraFee; }
+            set
+            {
+                if (value >= 0)
+                {
+                    extraFee = value;
+                }
+            }
+        }
+        public override decimal EstimatedCost
+        {
+            get
+            {
+                return DeliveryFee + (Weight * 5) + extraFee;
+            }
+        }
+        public ExpressShipment(string trackingCode, string description, decimal weight, decimal deliveryFee, DeliveryAddress destination, decimal ExtraFee) : base(trackingCode, description, weight, deliveryFee, destination)
+        {
+            ExtraFee = extraFee;
+        }
+
+
+    }
+}
