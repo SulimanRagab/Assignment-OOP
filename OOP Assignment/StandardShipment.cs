af27f2515208afc3d232be19dd454596f32c789c
Suliman <sulimanrageb015@gmail.com>
2026-08-09 16:45:02 +0300

    2. Create three shipment types (StandardShipment).

---
+++ OOP Assignment/StandardShipment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Assignment
+{
+    internal class StandardShipment : Shipment
+    {
+        public StandardShipment(string trackingCode, string description, double weight, double deliveryFee) : base(trackingCode, description, weight, deliveryFee)
+        {
+
+        }
+    }
+}
