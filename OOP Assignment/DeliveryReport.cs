71294db1cd5fa600735a093a72e42a7e65528fdb
Suliman <sulimanrageb015@gmail.com>
2026-08-17 16:04:24 +0300

    6 Create DeliveryReport

---
+++ OOP Assignment/DeliveryReport.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Assignment
+{
+    internal class DeliveryReport
+    {
+        public void PrintShipment(ITrackable shipment)
+        {
+            Console.WriteLine($"{shipment.GetTrackingStatus()}");
+        }
+        public void PrintInsurance(IInsurable shipment)
+        {
+            Console.WriteLine($"{shipment.CalculateInsurance()}");
+        }
+    }
+}
+
